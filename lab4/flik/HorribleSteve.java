2a15dacc2491dfe5efd1bfbd3ab00c19f272a631
Liaoliao Liu <liuliaoliao@bupt.cn>
2021-07-02 21:58:01 +0800

    complete lab4

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -1,9 +1,11 @@
 package flik;
+import static org.junit.Assert.assertTrue;
 
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
+            assertTrue(Flik.isSameNumber(128, 128));
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
